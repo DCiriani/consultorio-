e4894a1c5ede74ca561f789be69748fd07894365
Dciriani <diegociriani.psi@gmail.com>
2026-06-14 18:25:25 -0300

    fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Painel from "./Painel";
-import Cadastro from "./Cadastro";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Painel />} />
-      <Route path="/cadastro" element={<Cadastro />} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
